c3f2da60bd63e47d1c2d4ec7f2b5d5fa208858e9
luigi1015 <github@314ghz.com>
2014-01-08 13:11:33 -0600

    Test Commit 2. First one worked so lets see what it does with changes.

--- HelloWorld/src/HelloWorldApp.java
+++ HelloWorld/src/HelloWorldApp.java
@@ -3,6 +3,7 @@ public class HelloWorldApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");//Say Hello World!
+		System.out.println("Hiya World!");//Say Hiya World!
 
 	}
 
