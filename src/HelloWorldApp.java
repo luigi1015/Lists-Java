43bf9075a24004012d4dea1c0dd802b5ee15d218
luigi1015 <github@314ghz.com>
2014-01-09 14:51:41 -0600

    Created a test frame for the beginnings of a GUI.

--- src/HelloWorldApp.java
+++ src/HelloWorldApp.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 
 public class HelloWorldApp {
 
@@ -8,6 +10,9 @@ public class HelloWorldApp {
 		System.out.println("Testing from Ubuntu!");//Say Testing from Ubuntu!
 		//Test test test.
 
+		JFrame frame = new JFrame("Demo");//Creates the frame.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Sets the frame to exit on pressing the close button.
+		frame.setVisible(true);//Tell the frame to show itself.
 	}
 
 }
