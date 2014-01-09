dab2e5913077a0c779592fa2eb2d0538f9c284e4
luigi1015 <github@314ghz.com>
2014-01-08 19:10:49 -0600

    Moved the files around a bit and added an output line.

--- src/HelloWorldApp.java
+++ src/HelloWorldApp.java
@@ -4,6 +4,7 @@ public class HelloWorldApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");//Say Hello World!
 		System.out.println("Hiya World!");//Say Hiya World!
+		System.out.println("Hello World from Ubuntu!");//Say Hello World from Ubuntu!
 
 	}
 
