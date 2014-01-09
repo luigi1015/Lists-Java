199c569c486af9869a6f6b362c7f64e1e71a27f9
luigi1015 <github@314ghz.com>
2014-01-08 20:39:00 -0600

    Added an output line.

--- src/HelloWorldApp.java
+++ src/HelloWorldApp.java
@@ -5,6 +5,7 @@ public class HelloWorldApp {
 		System.out.println("Hello World!");//Say Hello World!
 		System.out.println("Hiya World!");//Say Hiya World!
 		System.out.println("Hello World from Ubuntu!");//Say Hello World from Ubuntu!
+		System.out.println("Testing from Ubuntu!");//Say Testing from Ubuntu!
 
 	}
 
