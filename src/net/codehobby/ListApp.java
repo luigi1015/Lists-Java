6373351cbb27ae47b66e07670556d361da2ec688
luigi1015 <github@314ghz.com>
2014-01-11 17:30:04 -0600

    Added a class called ListApp to run the application.

---
+++ src/net/codehobby/ListApp.java
@@ -0,0 +1,14 @@
+package net.codehobby;
+
+import javax.swing.JFrame;
+
+public class ListApp {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Demo");//Creates the frame.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Sets the frame to exit on pressing the close button.
+		frame.setVisible(true);//Tell the frame to show itself.
+
+	}
+
+}
