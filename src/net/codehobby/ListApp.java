3695e0a6cda3317a40ff1d4a4992d84e9adbc1c5
luigi1015 <github@314ghz.com>
2014-01-19 18:50:20 -0600

    Wrote a little Code for the GUI.

--- src/net/codehobby/ListApp.java
+++ src/net/codehobby/ListApp.java
@@ -1,14 +1,42 @@
 package net.codehobby;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class ListApp {
+public class ListApp extends JFrame
+{
+
+	private static final long serialVersionUID = 7438349917268570581L;//Automatically generated serial version ID.
+	
+	private JPanel panel;
+	
+	public ListApp()
+	{
+		initUI();
+	}
+
+	private void initUI()
+	{//Initialize the user interface elements.
+		//Set up the main panel.
+		panel = new JPanel();
+		getContentPane().add( panel );
+		panel.setLayout( null );;
+
+		setTitle( "List" );
+		setSize( 600, 600 );
+		setLocationRelativeTo( null );
+		setDefaultCloseOperation( EXIT_ON_CLOSE );//Set it to shut down once the close button is pressed.
+	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Demo");//Creates the frame.
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Sets the frame to exit on pressing the close button.
-		frame.setVisible(true);//Tell the frame to show itself.
-
+		//The invokeLater puts the application on the Swing Event Queue to make sure all UI updates are concurrency safe.
+		SwingUtilities.invokeLater( new Runnable() {
+			public void run() {
+				ListApp la = new ListApp();
+				la.setVisible( true );
+			}
+		});
 	}
 
 }
